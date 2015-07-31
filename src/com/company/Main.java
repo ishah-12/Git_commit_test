245eef527aed6d312d577dca51a1a0f0d78ef2d6
Ishan Shah <ishah@prosper.com>
2015-07-30 23:56:57 -0700

    Testing githooks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Testing git hooks");
+        System.out.println("Adding one more println");
 
 	// write your code here
     }
